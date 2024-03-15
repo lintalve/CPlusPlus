c8721af8de4c547e869be09029ee6696e9914356
lintalve <lintalve@icloud.com>
2024-03-15 16:46:43 +0100

    Initial Commit

---
+++ CPlusPlus/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CPlusPlus
+//
+//  Created by Volodymyr on 15.03.24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
